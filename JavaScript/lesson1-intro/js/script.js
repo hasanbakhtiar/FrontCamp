1781e8abce3eebd598673aaff57ee3e001ac5956
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-06-09 20:59:36 +0400

    js enter

---
+++ JavaScript/lesson1-intro/js/script.js
@@ -0,0 +1,24 @@
+// one line comment
+/**
+ * more 
+ * line 
+ * comment
+ */
+// document.write('Hello');
+
+
+// alert("Hello");
+// window.alert("Hello");
+
+debugger;
+
+console.log("Hello1");
+console.log("Hello2");
+console.log("Hello3");
+console.log("Hello4");
+console.log("Hello5");
+console.log("Hello6");
+console.log("Hello7");
+console.log("Hello8");
+console.log("Hello9");
+console.log("Hello10");
