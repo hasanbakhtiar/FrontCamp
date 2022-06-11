b6c98f49078698f8f81e4c758b3eafed38f11992
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-06-11 20:26:08 +0400

    operators and datatypes

---
+++ JavaScript/lesson2-datatypes/js/script.js
@@ -0,0 +1,52 @@
+// Primitive Group
+// String   DT(Data Type) 
+// var a = "hello";
+// console.log(a);
+// console.log("Hello2");
+
+
+
+
+// Number   DT(Data Type) 
+// var a = 5;
+// var b = 10;
+// console.log(a+b);
+// Boolean  DT(Data Type) 
+// true
+// false
+// if(false){
+//         console.log("duzdu");
+// }else{
+//     console.log('sehvdir');
+// }
+
+// Composite Group
+// Array    DT(Data Type) 
+// var info = [];
+// var info1 = new Array();
+// var info = ["table",'pen','book',1,2,3,true];
+// console.log(info);
+
+
+
+
+// Object   DT(Data Type) 
+// var info = {}
+// var info1 = new Object();
+// var info = {
+//     ad:"Samir",
+//     yas:22
+// }
+// console.log(info.yas);
+
+
+
+// Special Group
+// undefined DT(Data Type) 
+// var a = undefined; //tanimlanmamis
+// console.log(a);
+// null      DT(Data Type) 
+// var b = null; //bosh
+// console.log(b);
+// var a =null;
+// console.log(a);
---
+++ JavaScript/lesson3-opertors/js/script.js
@@ -0,0 +1,66 @@
+// Assingment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+// var a = 5;
+// a%=10;
+// console.log(a);
+
+// Arithmetic operators
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+// var a = 5;
+// a++;
+// console.log(a);
+
+// Comparison Operator 
+
+// < 
+// >
+// <=
+// >=
+// ==
+// ===
+// !=
+// !==
+// var a = "j";
+// var b = "a";
+// if (a>b) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// Logical operators
+// && - AND (false meyilli)
+// || - OR  (true meyilli) 
+// ! - NOT
+
+// if (!false ) {
+//     console.log("success");
+// }else{
+//     console.log('reject');
+// }
+
+var defaultUsername = "frontcamp";
+var defaultPass = "helo123";
+
+
+var enterUserName = "frontcamp";
+var enterPass = "helo1231";
+if ((defaultUsername === enterUserName) && (defaultPass === enterPass)) {
+    console.log(' login success');
+}else{
+    console.log('username or password wrong');
+}
+
