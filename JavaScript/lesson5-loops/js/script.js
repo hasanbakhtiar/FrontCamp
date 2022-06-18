d9aa65b5c4140426d661954049b9a02da44239e0
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-06-18 15:47:17 +0400

    funcctions

---
+++ JavaScript/lesson5-loops/js/script.js
@@ -0,0 +1,114 @@
+// while
+// let a = 0;
+// while (a<=10) {
+//     console.log(a);
+//     a++;
+// }
+
+// do while
+// let a = 1000;
+
+// do{
+//     console.log(a);
+//     a++;
+// }while(a<100);
+
+// for
+
+// for (let a = 0; a <= 10; a++) {
+//   console.log("Hello"+a);
+// }
+
+
+
+let foods =  ["🍎", "🍊", "🍗", "🍕", "🥩"];
+// foods; // ["🍎", "🍊", "🍗", "🍕", "🥩", "🍇"]
+// console.log(...foods);
+// for(let x = 0; x<5;x++){
+//     console.log("my foods:"+foods[x]);
+// }
+
+// for in
+// for(let x in foods){
+//     console.log(x+foods[x]);
+// }
+// for of
+
+// for( let a of foods){
+//     console.log(a);
+// }
+
+
+
+
+// ARRAY
+// const infoArr = new Array("Hello");
+// const infoArr = ['Hello',1,2,3];
+
+// arraylerde index deyerler 0dan baslayir
+// const infoArr = ['table','book','pen'];
+
+// infoArr[0] = 5;
+// infoArr[3] = "hello";
+
+
+// const infoArr = [];
+// infoArr[0] = 'table';
+// infoArr[1] = 'book';
+// infoArr[2] = 'pen';
+// infoArr['myitem'] = "new item";
+// console.log(infoArr['myitem']);
+
+
+// let table = "mable";
+// table = "nable";
+// const infoArr = [table]
+// infoArr[0] = "new";
+
+// const infoArr = ['table','pen',[1,3,4,[true],['hello','mello',[100,1000]],10]];
+
+
+// console.log(infoArr[2][4][1]);
+// console.log(infoArr[2][4][2][1]);
+
+
+// object
+
+// const infoObj = new Object({
+//     name:"Hello"
+// });
+
+// const infoObj = {
+//     name:"Hello"
+// }
+// console.log(infoObj.name);
+
+
+// const car = {
+//     allcar:[
+//         {
+//             brand:"Bmw",
+//             model:"x5",
+//             year:1990,
+//             new:false
+//         },
+//         {
+//             brand:"mer",
+//             model:"s200",
+//             year:1090,
+//             new:false
+//         }
+//     ]
+// }
+// car.allcar[0].brand = "a";
+
+
+// console.log(car.allcar[0].brand);
+
+
+
+
+
+
+
+
---
+++ JavaScript/lesson6-functions/js/script.js
@@ -0,0 +1,26 @@
+// function info (){
+//     console.log("Hello");
+// }
+
+// info();
+
+// function info(){
+//     return "Hello";
+// }
+// console.log(info());
+
+// const info = function(){
+//     return "hello";
+// }
+
+// console.log(info());
+// nan => not a number
+// function info(a=10,b=20){ //deafult
+//     return a+b;
+// }
+    
+// console.log(info(50,100));
+
+// (function info(a){
+//     console.log(a);
+// })("Hello")
